6ca6fbfac3c9558b4bb9319108845c2e616c23cc
Bret Doucette <doucette.bret@gmail.com>
2015-11-11 12:35:34 -0700

    removed caching for error purposes

--- app/models/departure.rb
+++ app/models/departure.rb
@@ -3,25 +3,19 @@ class Departure < ActiveRecord::Base
   validates :flight_id, presence: true, uniqueness: true
 
   def self.total_departures
-    Rails.cache.fetch('total', expires_in: 1.hour) do
-      count
-    end
+    count
   end
 
   def self.total_late_flights
-    Rails.cache.fetch('total_late_flights', expires_in: 1.hour) do
-      Departure.where(["dep_gate_delays > ?", 20]).sum
-    end
+    where(["dep_gate_delays > ?", 20]).count
   end
 
   def self.total_on_time_flights
-    Rails.cache.fetch('total_on_time_flights', expires_in: 1.hour) do
-      Departure.where(["dep_gate_delays < ?", 20]).sum
-    end
+    where(["dep_gate_delays < ?", 20]).count
   end
 
   def self.total_late_number(airline_id)
-    Departure.where("dep_gate_delays > ? AND airline_id = ?", 20, airline_id).count
+    where("dep_gate_delays > ? AND airline_id = ?", 20, airline_id).count
   end
 
   def self.total_late_percentage(airline_id)
