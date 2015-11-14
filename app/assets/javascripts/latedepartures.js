a26b8be2f8d215b67c7b12069bfac7295c0e8ca3
Bret Doucette <doucette.bret@gmail.com>
2015-11-14 11:19:56 -0700

    Fixed rewrite problem

--- app/assets/javascripts/latedepartures.js
+++ app/assets/javascripts/latedepartures.js
@@ -4,7 +4,7 @@ var lateDepartures = function(id) {
     url: 'http://localhost:3000/api/v1/latedepartures/' + id,
     dataType: 'json',
     success: function(response){
-      $('#airport_stats_3').append("<div class='col-md-4 project'><h3 id='counter2' style='margin-left: 20px;'>" + response + "</h3><h4 style='margin-left: 20px;'>Late Departures</h4></div>");
+      $('#airport_stats_3').html("<div class='col-md-4 project'><h3 id='counter2' style='margin-left: 20px;'>" + response + "</h3><h4 style='margin-left: 20px;'>Late Departures</h4></div>");
     }
   })
 }
--- app/assets/javascripts/totaldepartures.js
+++ app/assets/javascripts/totaldepartures.js
@@ -4,7 +4,7 @@ var totalDepartures = function(id) {
     url: 'http://localhost:3000/api/v1/totaldepartures/' + id,
     dataType: 'json',
     success: function(response){
-      $('#airport_stats_1').append("<div class='col-md-4 project'><h3 id='counter'>" + response + "</h3><h4>Total Flights</h4></div>");
+      $('#airport_stats_1').html("<div class='col-md-4 project'><h3 id='counter'>" + response + "</h3><h4>Total Flights</h4></div>");
     }
   })
 }
