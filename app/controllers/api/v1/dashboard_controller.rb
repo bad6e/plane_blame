db89f30863b0ac8f7b81337115036843af8c45fb
Bret Doucette <doucette.bret@gmail.com>
2015-11-15 14:56:12 -0700

    refactored airline ids

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -22,7 +22,7 @@ class Api::V1::DashboardController < ApplicationController
   end
 
   def airline_ids
-    [1,2,3,4,5,6,7,8]
+    Airline.pluck(:id)
   end
 
   def on_time_percentage
@@ -38,76 +38,6 @@ class Api::V1::DashboardController < ApplicationController
   end
 
 
-  def sw_on_time_percentage
-    Airport.find(params[:id]).departures.total_on_time_percentage(1)
-  end
-
-  def sw_number_flights
-    Airport.find(params[:id]).departures.total_flights_per_airline(1)
-  end
-
-  def ua_on_time_percentage
-    Airport.find(params[:id]).departures.total_on_time_percentage(2)
-  end
-
-  def ua_number_flights
-    Airport.find(params[:id]).departures.total_flights_per_airline(2)
-  end
-
-
-  def frontier_on_time_percentage
-    Airport.find(params[:id]).departures.total_on_time_percentage(3)
-  end
-
-  def frontier_number_flights
-    Airport.find(params[:id]).departures.total_flights_per_airline(3)
-  end
-
-
-  def delta_on_time_percentage
-    Airport.find(params[:id]).departures.total_on_time_percentage(4)
-  end
-
-  def delta_number_flights
-    Airport.find(params[:id]).departures.total_flights_per_airline(4)
-  end
-
-
-  def american_on_time_percentage
-    Airport.find(params[:id]).departures.total_on_time_percentage(5)
-  end
-
-  def american_number_flights
-    Airport.find(params[:id]).departures.total_flights_per_airline(5)
-  end
-
-
-
-  def jetblue_on_time_percentage
-    Airport.find(params[:id]).departures.total_on_time_percentage(6)
-  end
-
-  def jetblue_number_flights
-    Airport.find(params[:id]).departures.total_flights_per_airline(6)
-  end
-
-
-  def spirit_on_time_percentage
-    Airport.find(params[:id]).departures.total_on_time_percentage(7)
-  end
-
-  def spirit_number_flights
-    Airport.find(params[:id]).departures.total_flights_per_airline(7)
-  end
-
-
-  def virgin_on_time_percentage
-    Airport.find(params[:id]).departures.total_on_time_percentage(8)
-  end
-
-  def virgin_number_flights
-    Airport.find(params[:id]).departures.total_flights_per_airline(8)
-  end
 
   def last_update
     Airport.find(params[:id]).departures.last_updated_at
@@ -130,30 +60,6 @@ class Api::V1::DashboardController < ApplicationController
          number_of_flights: number_of_flights,
          airline_name: airline_name,
          # delay_index: delays,
-
-         sw_on_time_percentage: sw_on_time_percentage,
-         sw_number_flights: sw_number_flights,
-
-         ua_on_time_percentage: ua_on_time_percentage,
-         ua_number_flights: ua_number_flights,
-
-         frontier_on_time_percentage: frontier_on_time_percentage,
-         frontier_number_flights: frontier_number_flights,
-
-         delta_on_time_percentage: delta_on_time_percentage,
-         delta_number_flights: delta_number_flights,
-
-         american_on_time_percentage: american_on_time_percentage,
-         american_number_flights: american_number_flights,
-
-         jetblue_on_time_percentage: jetblue_on_time_percentage,
-         jetblue_number_flights: jetblue_number_flights,
-
-         spirit_on_time_percentage: spirit_on_time_percentage,
-         spirit_number_flights: spirit_number_flights,
-
-         virgin_on_time_percentage: virgin_on_time_percentage,
-         virgin_number_flights: virgin_number_flights,
         }
       }
     render json: to_json
