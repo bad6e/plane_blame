ecf51c692b8d7ed9413c5f52b408953894673a2a
Bret Doucette <doucette.bret@gmail.com>
2015-11-16 13:43:50 -0700

    Removed bad files

--- spec/controllers/api/v1/dashboard_controller.rb_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Api::V1::DashboardController.rbController, type: :controller do
-
-end
--- spec/controllers/api/v1/search_controller.rb_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Api::V1::SearchController.rbController, type: :controller do
-
-end
