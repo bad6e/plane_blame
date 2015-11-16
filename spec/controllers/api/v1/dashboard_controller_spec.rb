f0bf07592f3c36af90edee9bd3639d021c701d42
Bret Doucette <doucette.bret@gmail.com>
2015-11-16 15:57:11 -0700

    Added in vcr

--- spec/controllers/api/v1/dashboard_controller_spec.rb
+++ spec/controllers/api/v1/dashboard_controller_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::DashboardController, type: :controller do
 
+
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,3 +54,9 @@ RSpec.configure do |config|
 
   config.infer_spec_type_from_file_location!
 end
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.hook_into :webmock
+  c.allow_http_connections_when_no_cassette = true
+end
