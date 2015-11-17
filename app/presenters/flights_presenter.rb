636f9634eb0ac5ce81ecba1bac5ed3e986381362
Bret Doucette <doucette.bret@gmail.com>
2015-11-16 22:36:56 -0700

    Added best to worst airlines

--- app/presenters/flights_presenter.rb
+++ app/presenters/flights_presenter.rb
@@ -14,4 +14,20 @@ class FlightsPresenter
   def last_updated_at
     Departure.last_updated_at
   end
+
+  def airline_names
+    airline_ids.map {|r| Airline.find(r).name}
+  end
+
+  def total_percentage
+    airline_ids.map {|r| Departure.total_on_time_percentage(r)}
+  end
+
+  def total_percentage_and_name
+    total_percentage.zip(airline_names).sort { |x,y| y <=> x }
+  end
+
+  def airline_ids
+    Airline.pluck(:id)
+  end
 end
