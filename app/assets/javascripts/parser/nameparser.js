e426a217dd407e9f7bc186329bcceb98c120440e
Bret Doucette <doucette.bret@gmail.com>
2015-11-18 17:13:41 -0700

    Added in days of data

--- app/assets/javascripts/parser/nameparser.js
+++ app/assets/javascripts/parser/nameparser.js
@@ -1,4 +1,6 @@
 var nameParser = function(response) {
   var airportName = response['airport_name'].name
-  $('#airport_name').html("<h3>" + airportName + "</h3>");
+  var dataLength  = response['airport_name'].day_length
+  $('#airport_name').html("<h2>" + airportName + "</h2>");
+  $('#airline_data_range').html("<h4>Based on " + dataLength + " days of flight data</strong></h4></div>");
 }
