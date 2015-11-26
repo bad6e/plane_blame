d2446b22175fa60140dc2edca10c80667ac9dc98
Bret Doucette <doucette.bret@gmail.com>
2015-11-26 10:17:48 -0700

    Updated bug for gauge.js

--- app/assets/javascripts/airportinformation.js
+++ app/assets/javascripts/airportinformation.js
@@ -5,13 +5,11 @@ var airportInformation = function(id) {
     dataType: 'json',
     success: function(response){
       var scroll = $('#search_bar')[0];
-      $('#all_html').hide();
       nameParser(response);
       statsParser(response);
       delayindexParser(response);
       ontimeParser(response);
       $('#loaderImg').hide();
-      $('#all_html').fadeIn(1000)
       scroll.scrollIntoView();
     }
   })
