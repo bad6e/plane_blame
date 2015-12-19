18321b6139ce8b97ff2bd9d76cdb089827d74934
Bret Doucette <doucette.bret@gmail.com>
2015-12-19 11:29:14 -0700

    Added info warning

--- app/assets/javascripts/parser/delayindexparser.js
+++ app/assets/javascripts/parser/delayindexparser.js
@@ -4,5 +4,6 @@ var delayindexParser = function(response) {
   $('#delay_index').html("<div class='col-md-12 cBusiness'><h4><strong>Current Delay Index at " + airportName + "</strong></h4><div id='delay' class='gauge'></div></div>");
   gauge(delayIndex);
   $('#delay_key').html("<p>0 = No Delays 5 = You better bring a good book!</p>");
+  $('#delay_warn').html("**** THIS HAS BEEN DISABLED BECAUSE FLIGHT STATS API EVALUATION TRIAL ENDED ****");
   $('#airline_title').html("<h4><strong>Percentage of On-Time Departures for each Major Airline.</strong></h4>");
 }
